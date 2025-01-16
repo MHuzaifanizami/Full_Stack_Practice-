e00a3995047ce95292748c9fac2832d3993fc9d0
M Huzaifa Nizami <mhuzaifanizami@gmail.com>
2025-01-16 14:16:29 +0500

    server code completed

---
+++ Backend/index.mjs
@@ -0,0 +1,139 @@
+
+import express from 'express'
+import { send } from 'vite'
+const app = express()
+const port = 3000
+const products = []
+app.use(express.json())
+const todos = [];
+
+
+app.get('/get-todos', (req, res) => {
+  console.log("todo in get", req.body);
+
+  res.send(todos)
+})
+
+app.post('/post-todos', (req, res) => {
+
+  const todoObj = {
+    todo: req.body.todoContent,
+    id: Date.now()
+  }
+  todos.push(todoObj)
+  res.send('todo added successfully')
+})
+
+app.delete('/delete-todos/:id', (req, res) => {
+  const id = req.params.id
+  let isFound = false
+
+  for (let i = 0; i < todos.length; i++) {
+
+    if (todos[i].id == id) {
+      todos.splice(i, 1)
+      isFound = true
+      break
+    }
+  }
+
+  if (isFound) {
+    res.status(200).send("Todo deleted");
+  } else {
+
+    res.status(404).send("Todo not avaliable");
+  }
+
+})
+
+
+app.patch('/update-todos/:id', (req, res) => {
+  const id = req.params.id
+  let isFound = false
+  let todo = req.body.todo
+  console.log("todo updated", todo);
+
+  for (let i = 0; i < todos.length; i++) {
+
+    if (todos[i].id == id) {
+      todos[i].todo = todo
+      isFound = true
+      break
+    }
+  }
+
+  if (isFound) {
+    res.status(200).send("Todo updated");
+  } else {
+
+    res.status(404).send("Todo not avaliable");
+  }
+
+})
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/api/jokes', (req, res) => {
+
+
+  const jokes = [
+    {
+      id: 1,
+      title: 'hello 1',
+      description: 'hello 1'
+    },
+    {
+      id: 2,
+      title: 'hello 2',
+      description: 'hello 2'
+    },
+    {
+      id: 3,
+      title: 'hello 3',
+      description: 'hello 3'
+    },
+  ]
+
+  res.send(jokes)
+})
+
+
+app.post('/api/product', (req, res) => {
+  console.log("req", req.body);
+  const product = req.body
+  products.push(product)
+  res.send('Product added successfully')
+})
+
+app.get('/api/products', (req, res) => {
+  res.send(products)
+
+})
+
+app.get('/api/product/:id', (req, res) => {
+  const index = Number(req.params.id) - 1
+  const product = products[index]
+  if (!product) {
+    res.status(404).send({ message: "route not found!", status: 404 });
+
+  }
+
+  res.send({ data: product, message: 'Fetched Succcessfully', status: 200 })
+
+})
+
+
+app.use((req, res) => {
+  res.status(404).send({ message: "route not found!", status: 404 });
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+      .then((res) => {
+        setJokes(res.data)
+      })
+      .catch((error)=>{
+        console.log("error", error);
+
+      })
+  }, [])
+console.log("jokes" , jokes);
+
+  return (
+    <>
+      <h1>Hello world</h1>
+      <h1>JOkes : {jokes.length}</h1>
+
+
+      {
+        jokes.map((joke) => (
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.description}</p>
+          </div>
+        ))
+      }
+    </>
+  )
+}
+
+export default App
